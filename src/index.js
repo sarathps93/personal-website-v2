8b9e9a72390b9dbb15ed8047f1d84ca7a212a071
Sarath P S <sarathps93@gmail.com>
2020-12-12 00:53:07 +0530

    feat(*): removed unnecessary files and added service worker

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/common.css';
 import App from './App';
 
+import * as serviceWorker from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
